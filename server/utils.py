b2b8ffe8c727a520f720b75fc35c07d1bb383865
Leo Brdar <lbrdar@riteh.hr>
2017-08-21 00:02:58 +0200

    Extract functions for getting nodes & edges  to utils Add another algorithm

---
+++ server/utils.py
@@ -0,0 +1,30 @@
+from numpy import pi
+
+def get_nodes(network):
+    nodes = []
+    for n in network.nodes():
+        node = {
+            'id': n.id,
+            'x': network.pos[n][0],
+            'y': network.pos[n][1],
+            'theta': network.ori[n] * 180. / pi
+        }
+        nodes.append(node)
+    return nodes
+
+def get_edges(network):
+    edges = []
+    for e in network.edges():
+        n0 = e[0];
+        n1 = e[1];
+        edge = [{
+            'id': n0.id,
+            'x': network.pos[n0][0],
+            'y': network.pos[n0][1],
+        }, {
+            'id': n1.id,
+            'x': network.pos[n1][0],
+            'y': network.pos[n1][1],
+        }]
+        edges.append(edge)
+    return edges
--- server/views.py
+++ server/views.py
@@ -2,7 +2,8 @@ from django.views import View
 from django.shortcuts import render
 from pymote import *
 from pymote.algorithms.broadcast import Flood
-from numpy import pi
+from pymote.algorithms.readsensors import ReadSensors
+from utils import get_nodes, get_edges
 import inspect
 
 class Algorithm(View):
@@ -10,45 +11,23 @@ class Algorithm(View):
     component = 'algorithm'
     network = NetworkGenerator(100).generate_random_network()
 
-    def get_nodes(self, network):
-        nodes = []
-        for n in network.nodes():
-            node = {
-                'id': n.id,
-                'x': network.pos[n][0],
-                'y': network.pos[n][1],
-                'theta': network.ori[n] * 180. / pi
-            }
-            nodes.append(node)
-        return nodes
-
-    def get_edges(self, network):
-        edges = []
-        for e in network.edges():
-            n0 = e[0];
-            n1 = e[1];
-            edge = [{
-                'id': n0.id,
-                'x': network.pos[n0][0],
-                'y': network.pos[n0][1],
-            }, {
-                'id': n1.id,
-                'x': network.pos[n1][0],
-                'y': network.pos[n1][1],
-            }]
-            edges.append(edge)
-        return edges
-
     def get_network_dict(self, network):
         return {
-            'nodes': self.get_nodes(network),
-            'edges': self.get_edges(network),
+            'nodes': get_nodes(network),
+            'edges': get_edges(network),
         }
 
     def get(self, request):
         # gets passed to react via window.props
         props = {
-            'algorithm': inspect.getsource(Flood),
+            'algorithm': {
+                'label': 'Flood',
+                'code': inspect.getsource(Flood),
+            },
+            'algorithm2': {
+                'label': 'ReadSensors',
+                'code': inspect.getsource(ReadSensors),
+            },
             'network': self.get_network_dict(self.network)
         }
 
